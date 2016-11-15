96aa1bb9e2d9ed997bc32d78c83bc7647f21c0e8
ChrisMagnuson <hourglasssand@gmail.com>
2016-11-15 14:45:20 -0500

    Move Start-ParallelWork to module

---
+++ TervisPowerShellJobs.psm1
@@ -0,0 +1,30 @@
+﻿function Start-ParallelWork {
+    param (
+        $ScriptBlock,
+        $Parameters
+    )
+    $Jobs = @()
+
+    foreach ($Parameter in $Parameters) {
+        while ($(Get-Job -State Running | where Id -In $Jobs.Id | Measure).count -ge 10) { Start-Sleep -Milliseconds 100 }
+        $Jobs += Start-Job -ScriptBlock $ScriptBlock -ArgumentList $Parameter
+    }
+
+    while (
+        Get-Job -State Running | 
+        where Id -In $Jobs.Id
+    ) {
+        Write-Verbose "Sleeping for 100 milliseconds"
+        Start-Sleep -Milliseconds 100 
+    }
+    
+    $Results = Get-Job -HasMoreData $true | 
+    where Id -In $Jobs.Id |
+    Receive-Job
+
+    Get-Job -State Completed | 
+    where Id -In $Jobs.Id | 
+    Remove-Job
+    
+    $Results
+}
